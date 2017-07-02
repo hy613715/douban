23444821352d74dc5146ae6b91718f5c9dffd67a
huyuan <422018883@qq.com>
2017-07-03 00:04:27 +0800

    搜索功能

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,25 +1,38 @@
 <template>
-  <el-input
-    placeholder="请选择日期"
-    icon="search"
-    v-model="input2"
-    :on-icon-click="handleIconClick">
-  </el-input>
+  <div>
+    <el-input
+      placeholder="请选择日期"
+      icon="search"
+      v-model="input2"
+      :on-icon-click="handleIconClick">
+    </el-input>
+    <el-input
+      placeholder="请选择日期"
+      icon="search"
+      v-model="input1"
+      :on-icon-click="handleIconClick">
+    </el-input>
+  </div>
 </template>
 <script>
+
+import VueResource from 'vue-resource'
+import Vue from 'vue'
+
+Vue.use(VueResource)
+
 export default {
   data() {
     return {
-      input2: ''
+      input1: '',
+      input2: '',
+      searchData : ''
     }
   },
   methods: {
     getSearchData:function(){
-      debugger;
-      this.$http.get({
-        url:'/v2/movie/search?q = {'+ this.input2 +'}'
-      }).then(function(res){
-        this.searchData = res.body;
+        this.$http.get('/api/v2/movie/search?q='+this.input2+'&tag='+this.input1).then(res=>{
+          this.searchData = res.body;
       })
     },
     handleIconClick(ev) {
