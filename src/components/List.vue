20f6e2f2dd493654e73feb41ce6f5731ffad4772
zhouming <ming.zhou@qunar.com>
2017-07-02 14:07:01 +0800

    list

--- src/components/List.vue
+++ src/components/List.vue
@@ -11,16 +11,32 @@
 </style>
 
 <script>
-import Vue from 'vue'
 import VueResource from 'vue-resource'
+import Vue from 'vue'
 
 Vue.use(VueResource)
 
   export default {
     data() {
       return {
-
+          getUrl: 'https://api.douban.com/v2/movie/subject/1764796'
       }
+  },
+
+  mounted : function(){
+    this.getData();
+  },
+
+  methods: {
+    getData :function(){
+
+        this.$http.get(this.getUrl).then(res => {
+            console.log(res)
+
+      })
+
+
     }
+  }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -7,25 +7,5 @@ Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
-  render: h => h(App),
-
-  data: {
-    getUrl: 'https://api.douban.com/v2/movie/subject/1764796'
-  },
-
-  mounted : function(){
-    this.getData();
-  },
-
-  methods: {
-    getData :function(){
-        var _this = this;
-        _this.$http.get({
-            type: 'GET',
-            url: this.getUrl
-        }).then(function(res){
-            console.log(res)
-        })
-    }
-  }
+  render: h => h(App)
 })
