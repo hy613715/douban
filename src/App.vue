d68f80733eb033e1c1891838b6ac4073a34e4197
huyuan <422018883@qq.com>
2017-07-02 18:45:00 +0800

    数据渲染到页面上，还没有加样式，电影海报那里有一个报错，不知道什么原因

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
     <div id="app">
-        <el-row>
-            <el-col :span="16">
-                <topbar></topbar>
-            </el-col>
-            <el-col :span="8">
-                <list></list>
-                <search></search>
-            </el-col>
-        </el-row>
+        <list></list>
     </div>
 </template>
 
 <script>
-    import Topbar from './components/Topbar.vue'
-    import Search from './components/Search.vue'
     import List from './components/List.vue'
 
     export default {
         components: {
-            'topbar': Topbar,
-            'search': Search,
             'list': List
         }
     }
