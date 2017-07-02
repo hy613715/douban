907c0a08c99d841ca0df1d46c03c09a375d727c6
huyuan <422018883@qq.com>
2017-07-02 11:53:58 +0800

    删除不必要的文件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
     <div id="app">
-        <el-row>
-            <el-col :span="6">
-                <sidebar></sidebar>
-            </el-col>
-            <el-col :span="18">
-                <home33></home33>
-            </el-col>
-        </el-row>
+
     </div>
 </template>
 
 <script>
-    import Sidebar from './components/Sidebar.vue'
-    import Home from './components/Home.vue'
+
     export default {
         components: {
-          'sidebar' : Sidebar,
-          'home33' : Home
+
         }
     }
 </script>
